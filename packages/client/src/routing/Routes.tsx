d3c2033ec82a0d9176ed73ab4c481426f80cda42
Nilomiranda <nilomiranda3@gmail.com>
2021-11-28 22:22:11 -0300

    determining initial route based on session existence

--- packages/client/src/routing/Routes.tsx
+++ packages/client/src/routing/Routes.tsx
@@ -1,9 +1,37 @@
-import { Route, Switch } from 'react-router-dom'
+import { useQuery } from '@apollo/client'
+import { Route, Switch, useHistory } from 'react-router-dom'
 import { LoginPage } from '../auth/pages/LoginPage'
 import { RegisterPage } from '../auth/pages/RegisterPage'
+import { GET_SESSION } from '../auth/queries/Session'
 import { HomePage } from '../home/pages/HomePage'
+import { useToast } from '../providers/SnackbarProvider'
+
+const unauthenticatedPathNames = ['/login', '/register']
 
 const Routes = () => {
+  const history = useHistory()
+  const toast = useToast()
+
+  const { data, loading: loadingSession, error: sessionError } = useQuery(GET_SESSION)
+
+  const { session } = data || {}
+
+  const { location: { pathname } } = history
+
+
+  if (sessionError || (!loadingSession && !session && !unauthenticatedPathNames.includes(pathname))) {
+    toast.show({
+      kind: 'error',
+      message: 'You must log in first',
+    })
+
+    history.push('/login')
+  }
+
+  if (!loadingSession && session && unauthenticatedPathNames.includes(pathname)) {
+    history.push('/home')
+  }
+
   return (
     <Switch>
       {/* todo: Create component that defines initial page */}
