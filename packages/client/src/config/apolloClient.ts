f5a5345323bfd8d5abc5978cdedb4f345d6ba445
Nilomiranda <nilomiranda3@gmail.com>
2021-12-06 22:33:17 -0300

    fixed socket url

--- packages/client/src/config/apolloClient.ts
+++ packages/client/src/config/apolloClient.ts
@@ -1,10 +1,38 @@
 import {
   ApolloClient,
+  HttpLink,
   InMemoryCache,
+  split,
 } from "@apollo/client";
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from "@apollo/client/utilities";
+
+const httpLink = new HttpLink({
+  uri: String(import.meta.env.VITE_API_URL || ''),
+  credentials: 'include'
+})
+
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:3000/graphql',
+  options: {
+    reconnect: true
+  }
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
 
 export const client = new ApolloClient({
-  uri: String(import.meta.env.VITE_API_URL || ''),
+  link: splitLink,
   cache: new InMemoryCache(),
-  credentials: 'include',
 });
