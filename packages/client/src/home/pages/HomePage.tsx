376024f34dbefafbbf33d3c24b9ee838ec0380a4
Nilomiranda <nilomiranda3@gmail.com>
2021-12-09 22:52:16 -0300

    removed unused imports

--- packages/client/src/home/pages/HomePage.tsx
+++ packages/client/src/home/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, TextareaAutosize } from "@mui/material"
+import { Box } from "@mui/material"
 import { MessagesList } from "../../messages/components/MessagesList"
 import { NewMessageBox } from "../../messages/components/NewMessageBox"
 
